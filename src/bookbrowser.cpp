mvf/qolibri

commit 1f2a350c6861da05acf56dcc13272f4acb0aa71f

Fix subprocesses
- Pass arguments as QStringList to prevent splitting. This fixes
  external wave/mpeg for books with spaces in their title, such as
  "NHK 日本語発音アクセント辞典".
- Don't leak QProcess instances

src/bookbrowser.cpp
@@ -22,37 +22,36 @@ BookBrowser::BookBrowser(QWidget *parent)
 
 void BookBrowser::setSource(const QUrl &name)
 {
+    const QString path = name.path();
+    const QStringList args = name.query().split('?');
 
-    QStringList args = name.toString().split('?');
-
-    if (args[0] == "sound") {
-        // args[1] : wave file
+    if (path == "sound") {
+        // args[0] : wave file
         if (!CONF->waveProcess.isEmpty()) {
-            emit processRequested(CONF->waveProcess + ' ' + args[1]);
+            emit processRequested(CONF->waveProcess, args);
         } else if (!QAudioDeviceInfo::availableDevices(QAudio::AudioOutput).isEmpty()) {
-            emit soundRequested(args[1]);
+            emit soundRequested(args[0]);
         } else {
-            qWarning() << "Can't play sound" << CONF->waveProcess << args[1];
+            qWarning() << "Can't play sound" << CONF->waveProcess << args[0];
             emit statusRequested("Can't play sound");
         }
-    } else if (args[0] == "book" || args[0] == "menu") {
-        // args[1] : book index
-        // args[2] : page
-        // args[3] : offset
-        if ( args.count() == 4) {
-            int index = args[1].toInt();
+    } else if (path == "book" || path == "menu") {
+        // args[0] : book index
+        // args[1] : page
+        // args[2] : offset
+        if (args.count() == 3) {
+            const int index = args[0].toInt();
             if (index >= bookList_.count()) {
-                qWarning() << "Invalid book index" << args[1];
-                emit statusRequested("ERROR: Invalid book index: " + args[1]);
+                qWarning() << "Invalid book index" << args[0];
+                emit statusRequested("ERROR: Invalid book index: " + args[0]);
                 return;
             }
-            bool mflag = (args[0] == "menu") ? true : false;
             EB_Position pos;
-            pos.page = args[2].toInt();
-            pos.offset = args[3].toInt();
+            pos.page = args[1].toInt();
+            pos.offset = args[2].toInt();
 
             ReferencePopup *popup =
-                new ReferencePopup(bookList_[index], pos, this, mflag);
+                new ReferencePopup(bookList_[index], pos, this, path == "menu");
             connect(popup->bookBrowser(), SIGNAL(statusRequested(QString)),
                     SIGNAL(statusRequested(QString)));
             connect(popup->bookBrowser(),
@@ -70,18 +69,18 @@ void BookBrowser::setSource(const QUrl &name)
         } else {
             qWarning() << "Invalid Reference Parameter" << args.count();
         }
-    } else if (args[0] == "mpeg") {
-        // args[1] : mpeg file
+    } else if (path == "mpeg") {
+        // args[0] : mpeg file
         if (!CONF->mpegProcess.isEmpty()) {
-            emit processRequested(CONF->mpegProcess + ' ' + args[1]);
+            emit processRequested(CONF->mpegProcess, args);
         } else {
-            qWarning() << "Can't play moview";
-            emit statusRequested("Can't play Movie");
+            qWarning() << "Can't play movie" << CONF->mpegProcess << args[0];
+            emit statusRequested("Can't play movie");
         }
-    } else if (args[0] == "close") {
+    } else if (path == "close") {
         parentWidget()->close();
     } else {
-        qWarning() << "Invalid Command" << args[0];
+        qWarning() << "Invalid Command" << path;
     }
 }
 
@@ -137,7 +136,7 @@ void BookBrowser::contextMenuEvent(QContextMenuEvent* event)
             foreach(const char c, textCursor().selectedText().toUtf8()) {
                 addr += "%" + QString::number((ushort)((uchar)c), 16);
             }
-            emit processRequested(CONF->browserProcess + ' ' + addr);
+            emit processRequested(CONF->browserProcess, QStringList(addr));
         }
     }
     delete menu;

src/bookbrowser.h
@@ -29,7 +29,7 @@ signals:
     void searchRequested(SearchDirection, const QString &);
     //void menuRequested(Book*, int, int);
     void pasteRequested(const QString&);
-    void processRequested(const QString& prog);
+    void processRequested(const QString &program, const QStringList &arguments);
     void soundRequested(const QString& fname);
     void selectionRequested(const QString& str);
     void statusRequested(const QString &str);

src/bookview.cpp
@@ -114,8 +114,8 @@ RET_SEARCH BookView::newPage(QWidget *parent, const Query& query, bool newTab,
             SIGNAL(searchRequested(SearchDirection,QString)));
     connect(page->bookBrowser(), SIGNAL(pasteRequested(QString)),
             SIGNAL(pasteRequested(QString)));
-    connect(page->bookBrowser(), SIGNAL(processRequested(QString)),
-            SIGNAL(processRequested(QString)));
+    connect(page->bookBrowser(), SIGNAL(processRequested(QString, QStringList)),
+            SIGNAL(processRequested(QString, QStringList)));
     connect(page->bookBrowser(), SIGNAL(soundRequested(QString)),
             SIGNAL(soundRequested(QString)));
     connect(page, SIGNAL(selectionRequested(QString)),
@@ -179,8 +179,8 @@ RET_SEARCH BookView::newPage(QWidget *parent, const Query& query, bool newTab,
             WebPage *wpage = new WebPage(this, book->path(), query);
             connect(wpage, SIGNAL(loadFinished(bool)),
                     SLOT(webViewFinished(bool)));
-            connect(wpage, SIGNAL(linkRequested(QString)),
-                    SIGNAL(processRequested(QString)));
+            connect(wpage, SIGNAL(processRequested(QString, QStringList)),
+                    SIGNAL(processRequested(QString, QStringList)));
             connect(parent, SIGNAL(viewFontChanged(QFont)), wpage,
                     SLOT(changeFont(QFont)));
             connect(this, SIGNAL(popupBrowserSet(bool)), wpage,
@@ -208,8 +208,8 @@ RET_SEARCH BookView::newWebPage(const QString &name, const QString &url,
 {
     WebPage *wpage = new WebPage(this, url);
     connect(wpage, SIGNAL(loadFinished(bool)), SLOT(webViewFinished(bool)));
-    connect(wpage, SIGNAL(linkRequested(QString)),
-            SIGNAL(processRequested(QString)));
+    connect(wpage, SIGNAL(processRequested(QString, QStringList)),
+            SIGNAL(processRequested(QString, QStringList)));
     connect(this, SIGNAL(popupBrowserSet(bool)), wpage,
             SLOT(setPopupBrowser(bool)));
     emit popupBrowserSet(popup_browser);

src/bookview.h
@@ -52,10 +52,9 @@ private:
 signals:
     void searchRequested(SearchDirection, const QString&);
     void pasteRequested(const QString&);
-    void processRequested(const QString&);
+    void processRequested(const QString &program, const QStringList &arguments);
     void soundRequested(const QString&);
     void selectionRequested(const QString&);
-    //void linkRequested(const QString& prog);
     void popupBrowserSet(bool);
 
     void fontChanged(const QFont&);

src/mainwindow.cpp
@@ -928,7 +928,7 @@ void MainWindow::doSearch()
         foreach(const char c, str.toUtf8()) {
             addr += "%" + QString::number((ushort)((uchar)c), 16);
         }
-        execProcess(CONF->browserProcess + ' ' + addr);
+        execProcess(CONF->browserProcess, QStringList(addr));
     }
 }
 
@@ -953,14 +953,15 @@ void MainWindow::pasteSearchText(const QString &text)
     searchTextEdit->setText(text);
 }
 
-void MainWindow::execProcess(const QString &prog)
+void MainWindow::execProcess(const QString &program, const QStringList &arguments)
 {
-    QProcess *proc = new QProcess(this);
+    QProcess *const proc = new QProcess();
+    connect(proc, SIGNAL(finished(int, QProcess::ExitStatus)), proc, SLOT(deleteLater()));
     connect(proc, SIGNAL(error(QProcess::ProcessError)),
             SLOT(execError(QProcess::ProcessError)));
-    proc->start(prog);
+    proc->start(program, arguments);
 
-    QString msg = "Execute: " + prog;
+    QString msg = "Execute: " + program + ' ' + arguments.join(' ');
     showStatus(msg);
 }
 void MainWindow::execError(QProcess::ProcessError e)
@@ -1377,16 +1378,14 @@ void MainWindow::bookViewSlots()
             SLOT(viewSearch(SearchDirection,QString)));
     connect(bookView, SIGNAL(pasteRequested(QString)),
             SLOT(pasteSearchText(QString)));
-    connect(bookView, SIGNAL(processRequested(QString)),
-            SLOT(execProcess(QString)));
+    connect(bookView, SIGNAL(processRequested(QString, QStringList)),
+            SLOT(execProcess(QString, QStringList)));
     connect(bookView, SIGNAL(soundRequested(QString)),
             SLOT(execSound(QString)));
     connect(bookView, SIGNAL(selectionRequested(QString)),
             SLOT(changeOptSearchButtonText(QString)));
     connect(bookView, SIGNAL(allWebLoaded()),
             SLOT(setWebLoaded()));
-    //connect(bookView, SIGNAL(linkRequested(QString)),
-    //        SLOT(execProcess(QString)));
 }
 
 void MainWindow::groupDockSlots()

src/mainwindow.h
@@ -96,7 +96,7 @@ private slots:
     void changeOptSearchButtonText(const QString &str);
 
     void addMark();
-    void execProcess(const QString& prog);
+    void execProcess(const QString &program, const QStringList &arguments);
     void execSound(const QString& fname);
     void execError(QProcess::ProcessError);
     void checkSound();

src/webpage.cpp
@@ -18,7 +18,6 @@ WebPage::WebPage(QWidget *parent, const QString &url, const Query& query)
     connect(this, SIGNAL(loadProgress(int)), SLOT(progress(int)));
     connect(this, SIGNAL(linkClicked(const QUrl&)),
             SLOT(openLink(const QUrl&)));
-    //connect(this, SIGNAL(linkRequested(QString)), SLOT(execProcess(QString)));
 
     QByteArray enc = encString(url);
     QString ustr = setSearchString(url, enc, query.query);
@@ -74,7 +73,7 @@ WebPage::WebPage(QWidget *parent, const QString &url)
 }
 void WebPage::openNewWin()
 {
-    emit linkRequested(CONF->browserProcess + ' ' + hoveredLink);
+    emit processRequested(CONF->browserProcess, QStringList(hoveredLink));
 }
 
 void WebPage::copyHoveredLink(const QString &link, const QString&,
@@ -217,9 +216,7 @@ void WebPage::openLink(const QUrl &url)
         qDebug() << u.toString();
         load(u);
     } else {
-        emit linkRequested(CONF->browserProcess + ' ' + url.toString());
-        //emit linkRequested(CONF->browserProcess + ' ' +
-        //                   QString::fromAscii(url.toEncoded()));
+        emit processRequested(CONF->browserProcess, QStringList(url.toString()));
     }
 
 }

src/webpage.h
@@ -36,7 +36,7 @@ private slots:
     void setPopupBrowser(bool);
 
 signals:
-    void linkRequested(const QString& prog);
+    void processRequested(const QString &program, const QStringList &arguments);
 
 private:
     QByteArray encString(const QString &url);

commit 9f3b44fdd5db29401f4aedb94990da5c84b7d8dd

BookBrowser: Fix multimedia links not working in popups
Regression from 1f2a350c6861da05acf56dcc13272f4acb0aa71f

src/bookbrowser.cpp
@@ -59,8 +59,8 @@ void BookBrowser::setSource(const QUrl &name)
                     SIGNAL(searchRequested(SearchDirection,QString)));
             connect(popup->bookBrowser(), SIGNAL(pasteRequested(QString)),
                     SIGNAL(pasteRequested(QString)));
-            connect(popup->bookBrowser(), SIGNAL(processRequested(QString)),
-                    SIGNAL(processRequested(QString)));
+            connect(popup->bookBrowser(), SIGNAL(processRequested(QString, QStringList)),
+                    SIGNAL(processRequested(QString, QStringList)));
             connect(popup->bookBrowser(), SIGNAL(soundRequested(QString)),
                     SIGNAL(soundRequested(QString)));
             connect(popup->bookBrowser(), SIGNAL(selectionRequested(QString)),