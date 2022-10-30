mvf/qolibri

commit 6cd6dac7bc1e5ef3cbc698b014d1b826063feabd

MainWindow: Consolidate viewMenu(), viewFull()
Fixes #52

src/mainwindow.cpp
@@ -132,12 +132,12 @@ void MainWindow::createMenus()
     CONNECT_BUSY(enterAct);
     openBookAct = fmenu->addAction(QIcon(":/images/bookopen.png"),
                                    tr("&Open book"),
-                                   this, SLOT(viewMenu()), tr("Ctrl+O"));
+                                   [this] { viewPseudoSearch(MenuRead); }, tr("Ctrl+O"));
     CONNECT_BUSY(openBookAct);
 
     viewAllAct = fmenu->addAction(QIcon(":/images/find.png"),
                                   tr("&View all data"),
-                                  this, SLOT(viewFull()), tr("Ctrl+D"));
+                                  [this] { viewPseudoSearch(WholeRead); }, tr("Ctrl+D"));
     CONNECT_BUSY(viewAllAct);
 
     stopAct = fmenu->addAction(QIcon(":/images/stop.png"), tr("&Cancel"),
@@ -763,11 +763,11 @@ void MainWindow::focusSearch()
     searchTextEdit->selectAll();
 }
 
-void MainWindow::viewMenu()
+void MainWindow::viewPseudoSearch(SearchDirection direction)
 {
     SearchMethod m = model->method;
 
-    m.direction = MenuRead;
+    m.direction = direction;
     if (model->bookMode == ModeDictionary) {
         m.groupReader = model->method.group;
         m.bookReader = model->method.book;
@@ -783,22 +783,6 @@ void MainWindow::viewMenu()
         return;
     }
 
-    viewSearch(m.bookReader->name(), m);
-
-    if (model->bookMode == ModeDictionary) {
-        focusSearch();
-    }
-}
-
-void MainWindow::viewFull()
-{
-    SearchMethod m = model->method;
-
-    m.direction = WholeRead;
-    if (model->bookMode == ModeDictionary) {
-        m.groupReader = model->method.group;
-        m.bookReader = model->method.book;
-    }
     viewSearch(m.bookReader->name(), m);
     focusSearch();
 }

src/mainwindow.h
@@ -62,8 +62,7 @@ public slots:
 private slots:
     void connectClipboard(bool enable);
     void viewInfo(Book *book);
-    void viewMenu();
-    void viewFull();
+    void viewPseudoSearch(SearchDirection direction);
     void viewSearch();
     void viewSearch(const QString &str, const SearchMethod &method);
     void viewSearch(SearchDirection d, const QString &str);

commit 08476bb649767e653070dfe3764df84a9c5d6163

MainWindow: Fix "Read" menu items regression in group dock popup menu
Fixes: 6cd6dac7bc1e5ef3cbc698b014d1b826063feabd

src/mainwindow.cpp
@@ -1380,8 +1380,8 @@ void MainWindow::groupDockSlots()
             SLOT(viewInfo(Book*)));
     connect(groupDock, SIGNAL(fontViewRequested(Book*)),
             SLOT(setBookFont(Book*)));
-    connect(groupDock, SIGNAL(menuRequested()), SLOT(viewMenu()));
-    connect(groupDock, SIGNAL(fullRequested()), SLOT(viewFull()));
+    connect(groupDock, &GroupDock::menuRequested, [this] { viewPseudoSearch(MenuRead); });
+    connect(groupDock, &GroupDock::fullRequested, [this] { viewPseudoSearch(WholeRead); });
 }
 
 void MainWindow::setWebLoaded()